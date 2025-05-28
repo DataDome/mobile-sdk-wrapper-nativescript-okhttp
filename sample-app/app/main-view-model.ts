4e4eb644652570f7d1c95892eacbdccc0abf6dbf
rajyvandatadome <166418211+rajyvandatadome@users.noreply.github.com>
2025-05-28 11:17:26 +0200

    feat: add Android SDK native script wrapper
    * init nativescript project
    
    * add send http request ui
    
    * add android module
    
    * fix link to android module
    
    * add kotlin wrapper class
    
    * add make request and clear cookie implementation
    
    * add readme
    
    * clean up project
    
    * add codeowners

---
+++ sample-app/app/main-view-model.ts
@@ -0,0 +1,59 @@
+import { Observable } from '@nativescript/core'
+const coreApplication = require("@nativescript/core/application");
+declare const co: any
+
+export class HelloWorldModel extends Observable {
+  private _message: string
+
+  private dataDomeSDKWrapper: any
+
+  constructor() {
+    super()
+    this.updateMessage()
+
+    let androidApp = coreApplication.android.context
+    this.dataDomeSDKWrapper = new co.datadome.ns.wrapper.DataDomeSDKWrapper(androidApp, "key", "1.0.0", true)
+  }
+
+  get message(): string {
+    return this._message
+  }
+
+  set message(value: string) {
+    if (this._message !== value) {
+      this._message = value
+      this.notifyPropertyChange('message', value)
+    }
+  }
+
+  sendRequest() {
+    const completionHandler = new co.datadome.ns.wrapper.DataDomeSDKWrapper.CompletionHandler({
+        onSuccess(response: string) {
+        console.log("✅ Request succeeded:", response)
+        this.message = response
+      },
+        onError(error: string) {
+        console.error("❌ Request failed:", error)
+      }
+    })
+
+    const headers = new java.util.HashMap()
+    headers.put("User-Agent", "BLOCKUA")
+    headers.put("Accept", "application/json")
+
+    const body = JSON.stringify({
+      techno: "Nativescript",
+      role: "wrapper"
+    });
+
+    this.dataDomeSDKWrapper.makeRequest("post", "YOUR_ENDPOINT", headers, body, completionHandler)
+  }
+
+  clearCookie() {
+    this.dataDomeSDKWrapper.clearDataDomeCookie()
+  }
+
+  private updateMessage() {
+    this.message = ""
+  }
+}
